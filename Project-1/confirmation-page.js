5fce0aec78baca714b2f02b0eeac65a26bb5105e
atakee72 <atakee@gmail.com>
2023-04-21 17:49:33 +0200

    Styling and JS tweaks

--- Project-1/confirmation-page.js
+++ Project-1/confirmation-page.js
@@ -1,3 +1,50 @@
 let params = (new URL(document.location)).searchParams;
-let name = params.get('name'); // is the string "Jonathan Smith".
-let age = parseInt(params.get('age')); // is the number 18
+console.log("🚀 ~ params:", params);
+
+let email = params.get("email");
+console.log("🚀 ~ email:", email);
+const emailPlace = document.querySelector(".email")
+emailPlace.innerHTML = email
+
+let fname = params.get("fname");
+console.log("🚀 ~ fname:", fname);
+const fnamePlace = document.querySelector(".fname")
+fnamePlace.innerHTML = fname
+
+let sname = params.get("sname");
+console.log("🚀 ~ sname:", sname);
+const snamePlace = document.querySelector(".sname")
+snamePlace.innerHTML = sname
+
+let bday = params.get("bday");
+console.log("🚀 ~ bday:", bday);
+const bdayPlace = document.querySelector(".bday")
+bdayPlace.innerHTML = `born on ${bday}`
+
+let sex = params.get("sex");
+console.log("🚀 ~ sex:", sex);
+const sexPlace = document.querySelector(".sex")
+sexPlace.innerHTML = sex
+
+let travellerType = params.get("travellerType");
+console.log("🚀 ~ travellerType:", travellerType);
+const travellerTypePlace = document.querySelector(".type")
+travellerTypePlace.innerHTML = travellerType
+
+let textArea = params.get("textArea");
+console.log("🚀 ~ textArea:", textArea);
+const textAreaPlace = document.querySelector(".notes")
+textAreaPlace.innerHTML = `His/her note: ${textArea}`
+
+let checkBox = params.get("treatment");
+console.log("🚀 ~ checkBox:", checkBox);
+const checkboxPlace = document.querySelector(".treatment")
+checkboxPlace.innerHTML = checkBox
+
+
+
+// let age = parseInt(params.get("age")); // is the number 18
+// console.log("🚀 ~ age:", age);
+
+
+
